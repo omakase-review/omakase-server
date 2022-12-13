afccaa2e633882bb1a7bb671e411cbffdfc45429
ehgks0000 <ehgks0083@gmail.com>
2022-12-13 16:41:38 +0900

    Feature: Add Comments Schema
    1. User와 Review간의 Comments 추가
    2. Comment의 Sub Comment 기능 추가

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { createComment } from "../domains/review/controllers/create-comment";
 import { createFavorite } from "../domains/review/controllers/create-favorite";
 import { getRecentReviews } from "../domains/review/controllers/get-recent-reviews";
 import { getReviewById } from "../domains/review/controllers/get-review-by-id";
@@ -9,6 +10,7 @@ const reviewRouter = Router();
 
 reviewRouter.get("/", controllerHandler(getRecentReviews));
 reviewRouter.get("/:id", controllerHandler(getReviewById));
-reviewRouter.post("/:id/favorite", authJWT, controllerHandler(createFavorite));
+reviewRouter.post("/:id/favorite", controllerHandler(createFavorite));
+reviewRouter.post("/:id/comment", authJWT, controllerHandler(createComment));
 
 export { reviewRouter };
