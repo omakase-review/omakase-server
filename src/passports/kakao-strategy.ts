8eaebf1c2e56d5339edfdd63999da7636a4178a3
DoHan Kim <50390565+ehgks0000@users.noreply.github.com>
2022-12-25 22:58:02 +0900

    Feat: Add Auth 스키마 추가 (#10)
    1. User 스키마와 일대다인 Auth 추가하여 한 계정으로 여러 소셜 로그인이 가능하도록 하였습니다.

--- src/passports/kakao-strategy.ts
+++ src/passports/kakao-strategy.ts
@@ -1,26 +1,29 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
+import { AuthType } from "@prisma/client";
 import passport from "passport";
 import { Strategy as KakaoStrategy } from "passport-kakao";
 import { conf } from "../config";
 import prisma from "../db/prisma";
-import { createUser } from "../domains/user/services/create-user";
-import { getUserByEmail } from "../domains/user/services/get-user-by-email";
+import { createUserWithAuth } from "../domains/user/services/create-user-with-auth";
+import { getUserByAuth } from "../domains/user/services/get-user-by-auth";
 // import { BadReqError } from "../lib/http-error";
 
 export default () => {
     passport.use(
         new KakaoStrategy({ ...conf().KAKAO_CONFIG }, async (accessToken, _refreshToken, profile, cb) => {
-            const { has_email, email, name: _name } = profile._json.kakao_account;
+            const { has_email, email, name: _name, id } = profile._json.kakao_account;
             if (!has_email || !email) {
                 return cb(new Error("Not Found Email"));
                 // return cb(new BadReqError("Not Found Email"));
             }
 
-            const user = await getUserByEmail({ email }, prisma);
+            const type = AuthType.KaKao;
+
+            const user = await getUserByAuth({ email, type, socialId: id }, prisma);
 
             if (!user) {
-                const createdUser = await createUser({ email }, prisma);
+                const createdUser = await createUserWithAuth({ email,type, socialId: id }, prisma);
 
                 return cb(null, { ...createdUser, accessToken });
             }
